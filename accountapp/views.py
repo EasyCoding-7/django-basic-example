2c951ce482099fa8c99c050d975235f1aa1b429d
GoodayTH <taehyeongkim77@gmail.com>
2021-12-13 20:54:50 +0900

    Login / Logout

--- accountapp/views.py
+++ accountapp/views.py
@@ -25,8 +25,10 @@ def hello_world(request):
         hello_world_list = HelloWorld.objects.all()
         return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
 
+
 class AccountCreateView(CreateView):
     model = User
     form_class = UserCreationForm
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/create.html'
+
