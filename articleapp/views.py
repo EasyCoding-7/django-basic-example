879eccc61eef55e93c4ea8136c96083171eb0cff
GoodayTH <taehyeongkim77@gmail.com>
2021-12-14 20:34:00 +0900

    articleapp 구현 - Delete

--- articleapp/views.py
+++ articleapp/views.py
@@ -2,9 +2,9 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 # Create your views here.
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
 from django.utils.decorators import method_decorator
-from django.views.generic import CreateView, DetailView, UpdateView
+from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
 
 from articleapp.decorator import article_ownership_required
 from articleapp.forms import ArticleCreationForm
@@ -44,3 +44,12 @@ class ArticleUpdateView(UpdateView):
 
         def get_success_url(self):
             return reverse('articleapp:detail', kwargs={'pk': self.object.pk})
+
+@method_decorator(article_ownership_required, 'get')
+@method_decorator(article_ownership_required, 'post')
+class ArticleDeleteView(DeleteView):
+    model = Article
+    context_object_name = 'target_article'
+    template_name = 'articleapp/delete.html'
+    success_url = reverse_lazy('articleapp:list')
+
